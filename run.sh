d23f4d4769ae98c98317e6d15c08afd6a7f79294
Scott Cressi <scottcressi@gmail.com>
2020-12-29 21:15:17 -0500

    base, playbook var

--- run.sh
+++ run.sh
@@ -7,8 +7,10 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 
 echo "enter env: test | dev | prod"
 read -r ENV
+echo "enter playbook: base | test"
+read -r PLAYBOOK
 echo "enter group: git | mysql "
 read -r LIMIT
 
 ansible-galaxy install -r requirements.yaml -f
-ansible-playbook roles/test.yaml -e ansible_python_interpreter=/usr/bin/python2 -i inventories/inventory-"$ENV".yaml --limit "$LIMIT" --check --diff
+ansible-playbook roles/"$PLAYBOOK".yaml -e ansible_python_interpreter=/usr/bin/python2 -i inventories/inventory-"$ENV".yaml --limit "$LIMIT" --check --diff
