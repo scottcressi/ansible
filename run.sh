9c1db9df11eb3e89a746416985b0a2fb6eba7075
Scott Cressi <scottcressi@gmail.com>
2021-01-26 22:50:12 -0500

    exits

--- run.sh
+++ run.sh
@@ -45,8 +45,8 @@ while [ $# -gt 0 ]; do
     --env) ENV=$2 ; break ;;
     --limit) LIMIT=$2 ; break ;;
     --apply) NOOP= ; break ;;
-    --install-prereqs) install_prereqs ; break ;;
-    --test-ara) docker exec -ti ansible-ara sh -c "ara playbook list" ; break ;;
+    --install-prereqs) install_prereqs ; exit 0 ;;
+    --test-ara) docker exec -ti ansible-ara sh -c "ara playbook list" ; exit 0 ;;
     *) echo invalid option ; exit 0 ;;
   esac
   shift
