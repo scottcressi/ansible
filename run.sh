8e0b0a4d740fe0c4ddfe91002216e5203a36b16a
Scott Cressi <scottcressi@gmail.com>
2021-02-21 12:26:58 -0500

    ara

--- run.sh
+++ run.sh
@@ -98,8 +98,6 @@ done
 
 # ansible run analysis
 export ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)"
-export ARA_API_CLIENT="http"
-export ARA_API_SERVER="http://127.0.0.1:8000"
 
 # galaxy
 ansible-galaxy install -r requirements.yaml
