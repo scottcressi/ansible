be637dd3070e6f983d31fcb1f585ac4320f30b00
Scott Cressi <scottcressi@gmail.com>
2021-02-21 11:17:09 -0500

    echo

--- run.sh
+++ run.sh
@@ -113,4 +113,4 @@ echo galaxy
 ansible-galaxy install -r requirements.yaml
 
 echo ansible playbook run
-echo ansible-playbook "$PLAYBOOK" -i inventories/"$ENVIRONMENT".yaml --diff "$NOOP"
+ansible-playbook "$PLAYBOOK" -i inventories/"$ENVIRONMENT".yaml --diff "$NOOP"
